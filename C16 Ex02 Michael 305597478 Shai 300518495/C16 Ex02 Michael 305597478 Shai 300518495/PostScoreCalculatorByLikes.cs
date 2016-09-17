f5711b520b0a612a750ec85611c6b89bf212b841
levishai <levishai.g@gmail.com>
2016-09-18 01:41:41 +0300

    some post calc changes

---
+++ C16 Ex02 Michael 305597478 Shai 300518495/C16 Ex02 Michael 305597478 Shai 300518495/PostScoreCalculatorByLikes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C16_Ex02_Michael_305597478_Shai_300518495
+{
+    class PostScoreCalculatorByLikes : PostScoreCalculator
+    {
+        protected override int GetScore(int i_Likes, int i_Comments)
+        {
+            Console.WriteLine("");
+            return i_Likes;
+        }
+    }
+}
