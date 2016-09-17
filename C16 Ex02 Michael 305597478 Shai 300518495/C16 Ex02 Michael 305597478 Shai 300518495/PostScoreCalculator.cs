63249db4a6c40fd064e62d4c09a7cfea7be902df
Michael Kreimer <michaelkreimer@gmail.com>
2016-09-17 21:48:08 +0300

    postscorecalculator started implementation

---
+++ C16 Ex02 Michael 305597478 Shai 300518495/C16 Ex02 Michael 305597478 Shai 300518495/PostScoreCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C16_Ex02_Michael_305597478_Shai_300518495
+{
+    class PostScoreCalculator
+    {
+        BestPostFinder.eScoreCalculationStyle ScoreCalculationStyle {get;set ;}
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+
+        public PostScoreCalculator(BestPostFinder.eScoreCalculationStyle i_ScoreCalculationStyle,int i_LikesCount,int i_CommentsCount)
+        {
+            ScoreCalculationStyle = i_ScoreCalculationStyle;
+            LikesCount = i_LikesCount;
+            CommentsCount = i_CommentsCount;
+        }
+
+    }
+}
